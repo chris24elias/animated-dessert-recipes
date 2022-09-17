3f4c52d9a8e6dc17ad081a7875e07326bf6f0d34
Chris <chris24elias@gmail.com>
2022-09-17 18:03:47 -0400

    got a decent animation going

--- src/Navigation/index.tsx
+++ src/Navigation/index.tsx
@@ -8,7 +8,11 @@ const Stack = createNativeStackNavigator();
 const Navigation = ({}) => {
   return (
     <NavigationContainer>
-      <Stack.Navigator>
+      <Stack.Navigator
+        screenOptions={{
+          headerShown: false,
+        }}
+      >
         <Stack.Screen name="Home" component={Home} />
       </Stack.Navigator>
     </NavigationContainer>
--- src/screens/Home/index.tsx
+++ src/screens/Home/index.tsx
@@ -1,6 +1,12 @@
 import React, { PropsWithChildren, useRef } from "react";
-import { FlatList, Image, Text, View } from "react-native";
-import Animated from "react-native-reanimated";
+import { FlatList, Image, Text, useWindowDimensions, View } from "react-native";
+import Animated, {
+  Extrapolate,
+  interpolate,
+  useAnimatedScrollHandler,
+  useAnimatedStyle,
+  useSharedValue,
+} from "react-native-reanimated";
 import { RECIPES } from "../../utils";
 
 export type IHomeProps = {};
@@ -15,60 +21,118 @@ const useLazyRef = <T extends object>(initializer: () => T) => {
 };
 
 const Home = ({}: PropsWithChildren<IHomeProps>) => {
-  const y = useLazyRef(() => new Animated.Value(0));
-  const onScroll = useLazyRef(() =>
-    Animated.event(
-      [
-        {
-          nativeEvent: {
-            contentOffset: { y },
-          },
-        },
-      ],
-      { useNativeDriver: true }
-    )
-  );
+  const y = useSharedValue(0);
+
+  const scrollHandler = useAnimatedScrollHandler((event) => {
+    y.value = event.contentOffset.y;
+  });
   return (
     <AnimatedFlatList
       scrollEventThrottle={16}
-      bounces={false}
-      {...{ onScroll }}
+      // bounces={false}
+      onScroll={scrollHandler}
       data={RECIPES}
       renderItem={({ index, item }) => <RecipeCard {...{ index, y, item }} />}
       keyExtractor={(item) => `${item.id}`}
-      contentContainerStyle={{
-        paddingTop: 15,
-      }}
+      contentContainerStyle={
+        {
+          // paddingTop: 15,
+        }
+      }
     />
   );
 };
 
-const RecipeCard = ({ index, y, item }) => {
-  console.log("ITEM", item);
-  const imageSize = 180;
+const imageSize = 170;
+const CARD_HEIGHT_RAW = 200;
+const MARGIN = 15;
+const CARD_HEIGHT = CARD_HEIGHT_RAW + MARGIN * 2;
+
+const RecipeCard = ({
+  index,
+  y,
+  item,
+}: {
+  index: number;
+  y: Animated.SharedValue<number>;
+  item: any;
+}) => {
+  const { height } = useWindowDimensions();
+
+  const animatedStyle = useAnimatedStyle(() => {
+    const position = index * CARD_HEIGHT - y.value;
+    const isDisappearing = -CARD_HEIGHT;
+    const isTop = 0;
+    const isBottom = height - CARD_HEIGHT;
+    const isAppearing = height;
+
+    const a =
+      y.value +
+      interpolate(
+        y.value,
+        [0, 0.00001 + index * CARD_HEIGHT],
+        [0, -index * CARD_HEIGHT],
+        Extrapolate.CLAMP
+      );
+
+    const b = interpolate(
+      position,
+      [isBottom, isAppearing],
+      [0, -CARD_HEIGHT / 4],
+      Extrapolate.CLAMP
+    );
+
+    const translateY = a + b;
+    if (index === 1) {
+      console.log({ position, translateY, isAppearing, isDisappearing });
+    }
+
+    const scale = interpolate(
+      position,
+      [isDisappearing, isTop, isBottom, isAppearing],
+      [0.5, 1, 1, 0.5],
+      Extrapolate.CLAMP
+    );
+
+    const opacity = interpolate(
+      position,
+      [isDisappearing, isTop, isBottom, isAppearing],
+      [0.5, 1, 1, 0.5]
+    );
+
+    return {
+      opacity,
+      transform: [{ translateY }, { scale }],
+    };
+  });
+
   return (
-    <View
-      style={{
-        height: 220,
-        width: "90%",
-        borderRadius: 18,
-        shadowOffset: {
-          height: 2,
-          width: 0,
+    <Animated.View
+      style={[
+        animatedStyle,
+        {
+          height: CARD_HEIGHT_RAW,
+          width: "90%",
+          borderRadius: 18,
+          shadowOffset: {
+            height: 2,
+            width: 0,
+          },
+          shadowOpacity: 0.2,
+          shadowRadius: 4,
+          shadowColor: "#000000",
+          backgroundColor: item.bgColor,
+          // marginBottom: 30,
+          alignSelf: "center",
+          marginVertical: MARGIN,
         },
-        shadowOpacity: 0.2,
-        shadowRadius: 4,
-        shadowColor: "#000000",
-        backgroundColor: item.bgColor,
-        marginBottom: 30,
-        alignSelf: "center",
-      }}
+      ]}
     >
       <View style={{ flexDirection: "row", flex: 1 }}>
         <View style={{ flex: 1, padding: 15, justifyContent: "flex-end" }}>
           <Text
             style={{
-              fontSize: 20,
+              fontSize: 15,
               fontWeight: "600",
             }}
           >
@@ -98,7 +162,7 @@ const RecipeCard = ({ index, y, item }) => {
           />
         </View>
       </View>
-    </View>
+    </Animated.View>
   );
 };
 
